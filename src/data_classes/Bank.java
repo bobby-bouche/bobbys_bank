b09ad662ed11f4bb40cb2543e8d5d4e7f58f31c8
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2024-07-22 21:45:42 -0700

    created base bank and bank account classees

--- src/data_classes/Bank.java
+++ src/data_classes/Bank.java
@@ -1,5 +1,46 @@
 package data_classes;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import keyboard_classes.Keyboard;
+
 public class Bank {
+	
+	// bank fields
+	private Map<String, BankAccount> accounts;
+	private Keyboard kb;
+	
+	
+	// constructor
+	public Bank() {
+		accounts = new HashMap<>();
+		kb 		 = new Keyboard();
+	}
+
+
+	
+	// method to open new account
+	
+	
+	// method to delete account
+	
+	
+	// method to display account information
+	
+	
+	// method to dsplay all accounts
+	
+	
+	// method to save account informtion to a file
+	
+	
+
+	
+	@Override
+	public String toString() {
+		return "Bank [accounts=" + accounts + "]";
+	}
+
 
 }
--- src/data_classes/BankAccount.java
+++ src/data_classes/BankAccount.java
@@ -1,5 +1,93 @@
 package data_classes;
 
 public class BankAccount {
+	
+	// bankAccount fields
+	private int 	accNumber;
+	private String 	accType;
+	private double 	accBalance;
+	private String 	Fname;
+	private String 	LName;
+	private int 	age;
+	private String 	address;
+	
+	
+	// constructor
+	public BankAccount(final int accNum, final String accType, final String firstName, final String lastName, final  int age, String address, double balance) {
+		this.accNumber 	= accNum;
+		this.accType 	= accType;
+		this.Fname 		= firstName;
+		this.LName 		= lastName;
+		this.age 		= age;
+		this.address 	= address;
+		this.accBalance = balance;
+	}
 
+	
+	// getters
+	public int getAccNumber() {
+		return this.accNumber;
+	}
+	
+	public String getAccTyp() {
+		return this.accType;
+	}
+	
+	public String getFirstName() {
+		return this.Fname;
+	}
+	
+	public String getLastName() {
+		return this.LName;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	
+	public double getBalane() {
+		return this.accBalance;
+	}
+	
+	
+	
+	// method to deposit amount
+	public void depositAmount(double amount) {
+		if(amount <= 0) {
+			throw new IllegalArgumentException("Invalid amount to deposit: " + amount);
+		}
+		this.accBalance += amount;
+	}
+	
+	
+	// method to withraw amount
+	public void withdrawAmount(double amount) {
+		if(amount <= 0) {
+			throw new IllegalArgumentException("Invalid amount to withdraw: " + amount);
+		}
+		if(this.accBalance - amount < 0) {
+			throw new IllegalArgumentException("Insufficient funds: " + this.accBalance);
+		}
+		this.accBalance -= amount;
+	}
+	
+	
+	// method to transfer amount
+	public void transferAmount(double amount, BankAccount account) {
+		
+	}
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "BankAccount [accNumber=" + accNumber + ", accType=" + accType + ", accBalance=" + accBalance
+				+ ", Fname=" + Fname + ", LName=" + LName + ", age=" + age + ", address=" + address + "]";
+	}
+	
 }
